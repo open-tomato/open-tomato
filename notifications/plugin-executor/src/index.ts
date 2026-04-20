0ee0301cc8ce9f4ba22a51c30856c60cd970dd9f
Marcos Tomatti <markost@gmail.com>
2026-04-20 17:47:56 -0300

    refactor: relocate notifications/plugin-executor to packages/notifications

---
+++ notifications/plugin-executor/src/index.ts
@@ -0,0 +1,10 @@
+export type {
+  ExecutorEvent,
+  ApprovalRequest,
+  ApprovalDecision,
+} from './types.js';
+
+export {
+  ExecutorNotificationClient,
+  createExecutorNotificationClient,
+} from './client.js';
